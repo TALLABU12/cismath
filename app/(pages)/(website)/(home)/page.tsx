fa173564567dc683554792295049070fdacdc123
Talla Bu12 <talla@example.com>
2025-03-28 12:51:25 +0000

    1-netoyer ...

---
+++ app/(pages)/(website)/(home)/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+export default function Home() {
+  return (
+    <main className="">
+      hello
+      <Image
+        src="/assets/images/Untitled-removebg-preview.png"
+        width="200"
+        height="200"
+        alt="logo"
+      />
+    </main>
+  );
+}
